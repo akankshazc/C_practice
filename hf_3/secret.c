55bae0217fd32260dea0f8d313a447f5f57679a2
Akanksha <akankshazc23@icloud.com>
2025-09-16 15:40:01 +0530

    Add secret program to demonstrate word input processing and output redirection

---
+++ hf_3/secret.c
@@ -0,0 +1,18 @@
+// Chapter 3
+
+#include <stdio.h>
+
+int main()
+{
+    char word[10];
+    int i = 0;
+    while (scanf("%9s", word) == 1)
+    {
+        i = i + 1;
+        if (i % 2) // odd words to message1.txt
+            fprintf(stdout, "%s\n", word);
+        else // even words to message2.txt
+            fprintf(stderr, "%s\n", word);
+    }
+    return 0;
+}
