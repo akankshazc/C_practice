02a9148e79568c2cadd11e2627663475b60593a3
Akanksha <akankshazc23@icloud.com>
2025-09-20 22:35:35 +0530

    Add myrtle program to demonstrate struct usage in C

---
+++ hf_5/myrtle.c
@@ -0,0 +1,24 @@
+// HF Chapter 5
+
+#include <stdio.h>
+
+typedef struct
+{
+    const char *name;
+    const char *species;
+    int age;
+} turtle;
+
+void happy_birthday(turtle *t)
+{
+    (*t).age = (*t).age + 1; // Can alse use t->age instead of (*t).age
+    printf("Happy Birthday %s! You are now %i years old!\n", (*t).name, (*t).age);
+}
+
+int main()
+{
+    turtle myrtle = {"Myrtle", "Leatherback sea turtle", 99};
+    happy_birthday(&myrtle);
+    printf("%s's age is now %i\n", myrtle.name, myrtle.age);
+    return 0;
+}
