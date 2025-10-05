f2d10d89e15927bec81435e9c6ca00d8cb48592d
Akanksha <akankshazc23@icloud.com>
2025-10-05 22:11:28 +0530

    Add escape sequence demonstration program

---
+++ escape_seq.c
@@ -0,0 +1,13 @@
+// packt: Chapter 2
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, world without a new line");
+    printf("Hello, world with a new line\n");
+    printf("A string with \"quoted test\" inside of it\n\n");
+    printf("Tabbed\tColumn\tHeadings\n");
+    printf("A line of text that\n spans three lines\n and completes the line\n\n");
+    return 0;
+}
