a3359d6628511cf83f9646dceda32cd8d7bdccff
Akanksha <akankshazc23@icloud.com>
2025-10-06 06:37:00 +0530

    Add checksum function implementation in new source file

---
+++ checksum.c
@@ -0,0 +1,14 @@
+// HF Chapter 8
+
+#include "checksum.h"
+
+int checksum(char *message)
+{
+    int c = 0;
+    while (*message)
+    {
+        c += c ^ (int)(*message);
+        message++;
+    }
+    return c;
+}
